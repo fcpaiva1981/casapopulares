5846a3086d5f1d2f068abcb9a95f0f4fba79df1f
Fernando Campos de Paiva <97798726+fcpaiva1981@users.noreply.github.com>
2023-03-22 11:25:35 -0300

    Application
    FIM

--- casas-populares/application/src/main/java/br/com/fcpaiva/admin/application/familia/create/DefaultCreateFamiliaUseCase.java
+++ casas-populares/application/src/main/java/br/com/fcpaiva/admin/application/familia/create/DefaultCreateFamiliaUseCase.java
@@ -1,6 +1,6 @@
 package br.com.fcpaiva.admin.application.familia.create;
 
-import br.com.fcpaiva.admin.application.familia.retrieve.get.FamiliaOutput;
+
 import br.com.fcpaiva.admin.domain.familia.Familia;
 import br.com.fcpaiva.admin.domain.familia.FamiliaGateway;
 import br.com.fcpaiva.admin.domain.validation.handler.Notification;
@@ -10,16 +10,15 @@ import java.util.Objects;
 
 import static io.vavr.API.Left;
 import static io.vavr.API.Try;
-import static jdk.internal.ref.Cleaner.create;
 
 public class DefaultCreateFamiliaUseCase extends CreateFamiliaUseCase {
 
     private final FamiliaGateway familiaGateway;
 
     public DefaultCreateFamiliaUseCase(final FamiliaGateway familiaGateway) {
-
         this.familiaGateway = Objects.requireNonNull(familiaGateway);
     }
+    @Override
     public Either<Notification, CreateFamiliaOutput> execute(final CreateFamiliaCommand aCommand) {
         final var nomePai = aCommand.nomePai();
         final var nomeMae = aCommand.nomeMae();
@@ -33,13 +32,12 @@ public class DefaultCreateFamiliaUseCase extends CreateFamiliaUseCase {
         final var aFamilia = Familia.novaFamilia(nomePai, nomeMae, renda, pontuacao, dependentes, ativo);
         aFamilia.validate(notification);
 
-        return notification.hasError() ? Left(notification) : create(aCategory);
+        return notification.hasError() ? Left(notification) : create(aCommand);
     }
 
-    @Override
-    public Either<Notification, CreateFamiliaOutput> execute(CreateFamiliaUseCase anIn) {
+    private Either<Notification, CreateFamiliaOutput> create(final Familia aFamilia) {
         return Try(() -> this.familiaGateway.create(aFamilia))
                 .toEither()
-                .bimap(Notification::create, FamiliaOutput::from);
+                .bimap(Notification::create, CreateFamiliaOutput::from);
     }
 }
